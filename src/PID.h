a6ed8d773792a5152bc109a18c81575c6a46c947
laljarus <laljarus@gmail.com>
2018-06-08 16:32:11 +0200

    updated

--- src/PID.h
+++ src/PID.h
@@ -9,6 +9,8 @@ public:
   double p_error;
   double i_error;
   double d_error;
+  double prev_d_error;
+  
 
   /*
   * Coefficients
@@ -16,6 +18,7 @@ public:
   double Kp;
   double Ki;
   double Kd;
+  double Tf;
 
   /*
   * Constructor
@@ -30,12 +33,12 @@ public:
   /*
   * Initialize PID.
   */
-  void Init(double Kp, double Ki, double Kd);
+  void Init(double Kp, double Ki, double Kd,double N);
 
   /*
   * Update the PID error variables given cross track error.
   */
-  void UpdateError(double cte);
+  void UpdateError(double cte,double dt);
 
   /*
   * Calculate the total PID error.
